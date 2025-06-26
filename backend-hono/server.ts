c8649cfce154bacae6cebdcc49e1022b263e0262
Adithya Hegde Kota <hegdeadithyak@gmail.com>
2025-06-26 16:25:30 +0530

    Add wrangler config for Hono backend

--- backend-hono/server.ts
+++ backend-hono/server.ts
@@ -4,6 +4,7 @@ import { logger } from 'hono/logger'
 import chat from './api/chat'
 import memory from './api/memory'
 import blog from './api/blog'
+import retell from './api/retell'
 
 export const app = new Hono()
 
@@ -13,5 +14,6 @@ app.use('*', cors())
 app.route('/chat', chat)
 app.route('/memory', memory)
 app.route('/blog', blog)
+app.route('/retell', retell)
 
 export default app
